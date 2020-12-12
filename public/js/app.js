b848074a4f9058132876ae54bf33518dfcb6a7ad
Sadreddin Mahmoodkalayeh <sadreddin.mahmoodkalayeh@gmail.com>
2020-12-13 02:16:00 +0330

    add new weather info at /

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,16 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-01");
 const messageTwo = document.querySelector("#message-02");
+const messageThree = document.querySelector("#message-03");
+const messageFour = document.querySelector("#message-04");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
+  messageThree.textContent = "";
+  messageFour.textContent = "";
 
   const address = search.value;
 
@@ -17,11 +21,9 @@ weatherForm.addEventListener("submit", (e) => {
         messageOne.textContent = data.error;
       } else {
         messageOne.textContent = data.location;
-        messageTwo.textContent =
-          data.forecastData.weather +
-          ", " +
-          data.forecastData.temperature +
-          " C";
+        messageTwo.textContent = data.forecastData.weather;
+        messageThree.textContent = `Temperature: ${data.forecastData.temperature} C,   Feels like: ${data.forecastData.feels_like} C`;
+        messageFour.textContent = `Humidity: ${data.forecastData.humidity},   wind: ${data.forecastData.wind}`;
       }
     });
   });
